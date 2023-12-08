579c3e6bb4212ae1b8d7f208b8935dd58d00ade6
AdrienneUsi <usiadrienne35@gmail.com>
2023-12-08 15:13:52 -0800

    did methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Excercise 1
+            //Name: Adrienne
+            //From: California
+            //Singer: Khelani
+            //Song: Good Thing 
+
+            Console.WriteLine("Greetings! What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {userName} Where are you from?");
+            var from = Console.ReadLine();
+
+            Console.WriteLine($"Wow! {from} is a great place! Who is your favorite singer?");
+            var singer = Console.ReadLine();
+
+            Console.WriteLine($"{singer} is a extrodinary artist. What is your favorite song?");
+            var song = Console.ReadLine();
+
+            Console.WriteLine($"Thanks {userName} for answering these questions.These are some fun facts about you!");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"From: {from}");
+            Console.WriteLine($"Favorite Singer:{singer}");
+            Console.WriteLine($"Favirite Song:{song}");
+
+        } }
+
         }
-    }
-}
+
+        
+   
+    
